a004b8fe3191b642a19a8c50c92abfb17c676301
Andy Cowell <andy@cowell.org>
2017-10-04 17:17:47 -0400

    seemed to work

--- cloudformation/launch-vpc.sh
+++ cloudformation/launch-vpc.sh
@@ -3,10 +3,6 @@ export PROFILE=knoxpy
 export VPCNAME=knoxpyvpc
 export CIDR=192.168.0.0/21
 export PUBSUB0=192.168.0.0/24
-export PUBSUB1=192.168.5.0/24
-export PUBSUB2=192.168.2.0/24
-export PUBSUB3=192.168.3.0/24
-export PUBSUB4=192.168.4.0/24
 
 #
 # Find the number of AZs
@@ -18,34 +14,8 @@ export NUMAZS=`aws  ec2 describe-availability-zones  --profile ${PROFILE} --regi
 #
 export SUBNETPARAMS=$(cat <<EOF
             ParameterKey=PublicSubnetCIDR0,ParameterValue=${PUBSUB0}
-            ParameterKey=PublicSubnetCIDR1,ParameterValue=${PUBSUB1}
-
 EOF
 )
-
-if [ $NUMAZS -gt 2 ] ; then
-    SUBNETPARAMS=$(cat <<EOF
-${SUBNETPARAMS}
-            ParameterKey=PublicSubnetCIDR2,ParameterValue=${PUBSUB2}
-EOF
-)
-fi
-
-if [ $NUMAZS -gt 3 ] ; then
-    SUBNETPARAMS=$(cat <<EOF
-${SUBNETPARAMS}
-            ParameterKey=PublicSubnetCIDR3,ParameterValue=${PUBSUB3}
-EOF
-)
-fi
-
-if [ $NUMAZS -gt 4 ] ; then
-    SUBNETPARAMS=$(cat <<EOF
-${SUBNETPARAMS}
-            ParameterKey=PublicSubnetCIDR4,ParameterValue=${PUBSUB4}
-EOF
-)
-fi
 
 
 #
